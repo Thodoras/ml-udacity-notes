e1d9b717c077eed7df9800d27f812caa1fc2d9d4
thodoras <theodoretopol@gmail.com>
2020-12-10 14:41:24 +0200

    logistic

---
+++ logistic.py
@@ -0,0 +1,17 @@
+# Activation (sigmoid) function
+def sigmoid(x):
+    return 1/(1 + np.exp(x))
+
+# Output (prediction) formula
+def output_formula(features, weights, bias):
+    return sigmoid(np.dot(features, weights) + bias)
+
+# Error (log-loss) formula
+def error_formula(y, output):
+    return -y*np.log(output) - (1-y)*np.log(1-output)
+
+# Gradient descent step
+def update_weights(x, y, weights, bias, learnrate):
+    weights -= learnrate*x*(y - output_formula(x, weights, bias))
+    bias -= learnrate*(y - output_formula(x, weights, bias))
+    return weights, bias
